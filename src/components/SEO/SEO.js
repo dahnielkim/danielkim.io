e3fee67022d3af9128ae6cf9fe3f7ed81346518a
Daniel Kim <dkim@vlocity.com>
2019-03-01 11:21:13 -0800

    Added google analytics

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -77,7 +77,20 @@ function SEO({ meta, image, title, description, slug, lang = "en" }) {
                                     : []
                             )
                             .concat(meta)}
-                    />
+                    >
+                        <script
+                            async
+                            src="https://www.googletagmanager.com/gtag/js?id=UA-135482517-1"
+                        />
+                        <script>
+                            {`
+                            window.dataLayer = window.dataLayer || []; function
+                            gtag(){dataLayer.push(arguments)}
+                            gtag('js', new Date()); gtag('config',
+                            'UA-135482517-1');
+                            `}
+                        </script>
+                    </Helmet>
                 );
             }}
         />
