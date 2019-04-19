794bd3869badcdfe5baa1cdd8b5cc51b798955cc
Daniel Kim <dkim@vlocity.com>
2019-04-19 01:53:19 -0400

    Updated formatReadingTime util and resume

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,9 +4,15 @@
  * @returns {String}
  */
 export function formatReadingTime(minutes) {
-  const numCups = Math.round(minutes / 5);
+  let cups = Math.round(minutes / 5);
 
-  return ` / ${new Array(numCups || 1).fill('☕️').join('')} min read`;
+  if (cups > 5) {
+    return ` / ${new Array(Math.round(cups / Math.E))
+      .fill('🍱')
+      .join('')} ${minutes} min read`;
+  } else {
+    return ` / ${new Array(cups || 1).fill('☕️').join('')} ${minutes} min read`;
+  }
 }
 
 /**
